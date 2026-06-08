05723c3383dc36efe92ce0d459155409fbfe7278
VEDITH VANAM <vanamvedith@gmail.com>
2026-06-08 18:57:11 +0530

    Add: is-subsequence - submission-2

---
+++ Data Structures & Algorithms/is-subsequence/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        #using two pointers
+        spointer = 0
+        tpointer = 0
+        while spointer < len(s) and tpointer < len(t):
+            if s[spointer] == t[tpointer]:
+                spointer +=1
+            tpointer += 1
+        return spointer == len(s)
