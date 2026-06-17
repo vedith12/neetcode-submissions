d5d4aaa1b46f54f471b376f88bad76a6d5b3f6c2
VEDITH VANAM <vanamvedith@gmail.com>
2026-06-17 17:58:17 +0530

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l = 0
+        r = x
+        res = 0
+        while l<= r:
+            m = (l+r)//2
+            if m**2 >x:
+                r = m-1
+            elif m**2 <x:
+                l = m+1
+                res = m
+            else:
+                return m
+        return res
