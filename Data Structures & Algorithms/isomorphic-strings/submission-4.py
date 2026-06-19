9b94e9c7c185a4e40841a91e80da5f1872ee30ef
VEDITH VANAM <vanamvedith@gmail.com>
2026-06-19 21:12:29 +0530

    Add: isomorphic-strings - submission-4

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        hashmapS = {}
+        hashmapT = {}
+        for i in range(len(s)):
+            chS, chT = s[i], t[i]
+            if (chS in hashmapS and hashmapS[chS] != chT) or (chT in hashmapT and hashmapT[chT] != chS):
+                return False
+            hashmapS[chS] = chT
+            hashmapT[chT] = chS
+        return True
